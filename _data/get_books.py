815300fbe33cc8ad4c07864e7caf8e5f5a530e4e
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2023-07-27 11:39:24 -0400

    cleanup

--- _data/get_books.py
+++ _data/get_books.py
@@ -10,12 +10,14 @@ soup = BeautifulSoup(page.content, 'html.parser')
 books = soup.find_all('tr', class_='book-preview')
 
 data = []
-for book in books[:3]:  # only take the first 3 books
+for book in books[:5]:  # only take the first 5 books
     title_link = book.find('td', {'data-title': 'Title'}).find('a')
     title = title_link.text
     link = "https://bookwyrm.social" + title_link['href']
+    author_link = book.find('td', {'data-title': 'Author'}).find('a')
+    author = author_link.text
     img_url = book.find('img', class_='book-cover')['src']
-    data.append({'title': title, 'link': link, 'img_url': img_url})
+    data.append({'title': title, 'link': link, 'img_url': img_url, 'author': author})
 
 with open('assets/read.json', 'w') as f:
     json.dump(data, f)
@@ -33,8 +35,10 @@ for book in books:
     title_link = book.find('td', {'data-title': 'Title'}).find('a')
     title = title_link.text
     link = "https://bookwyrm.social" + title_link['href']
+    author_link = book.find('td', {'data-title': 'Author'}).find('a')
+    author = author_link.text
     img_url = book.find('img', class_='book-cover')['src']
-    data.append({'title': title, 'link': link, 'img_url': img_url})
+    data.append({'title': title, 'link': link, 'img_url': img_url, 'author': author})
 
 with open('assets/reading.json', 'w') as f:
     json.dump(data, f)
