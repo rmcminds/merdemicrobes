8273efa9dcc35a3db3902c53270339983176b8e2
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2023-07-29 00:07:14 -0400

    bookwyrm dates and ratings

--- scripts/get_books.py
+++ scripts/get_books.py
@@ -1,44 +1,63 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 import json
 
-URL = 'https://bookwyrm.social/user/rmcminds/books/read?sort=-finish_date'
-page = requests.get(URL)
+read_url = 'https://bookwyrm.social/user/rmcminds/books/read?sort=-finish_date'
+reading_url = 'https://bookwyrm.social/user/rmcminds/books/reading?sort=-start_date'
 
-soup = BeautifulSoup(page.content, 'html.parser')
+payload = {
+    'localname': sys.argv[1],
+    'password': sys.argv[2]
+}
 
-books = soup.find_all('tr', class_='book-preview')
+# log in and retrieve html from both read and reading pages
+with requests.Session() as s:
+    payload['csrfmiddlewaretoken'] = s.get('https://bookwyrm.social/').cookies['csrftoken']
+    p = s.post('https://bookwyrm.social/login?next=/', data=payload)
+    read_books_page = s.get(read_url)
+    reading_books_page = s.get(reading_url)
 
+# parse read books html
+read_books = BeautifulSoup(read_books_page.content, 'html.parser').find_all('tr', class_='book-preview')
 data = []
-for book in books[:5]:  # only take the first 5 books
+for book in read_books:
     title_link = book.find('td', {'data-title': 'Title'}).find('a')
     title = title_link.text
     link = "https://bookwyrm.social" + title_link['href']
-    author_link = book.find('td', {'data-title': 'Author'}).find('a')
-    author = author_link.text
+    author = book.find('td', {'data-title': 'Author'}).find('a').text
+    date_finished = book.find('td', {'data-title': 'Started'}).nextSibling.text.strip('\n ')
+    rating = book.find('td', {'data-title': 'Rating'}).text.strip('\n ')
     img_url = book.find('img', class_='book-cover')['src']
-    data.append({'title': title, 'link': link, 'img_url': img_url, 'author': author})
+    data.append({
+      'title': title,
+      'link': link,
+      'img_url': img_url,
+      'author': author,
+      'date_finished': date_finished,
+      'rating': rating,
+    })
 
 with open('_data/read.json', 'w') as f:
     json.dump(data, f)
 
-
-URL = 'https://bookwyrm.social/user/rmcminds/books/reading?sort=-start_date'
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-books = soup.find_all('tr', class_='book-preview')
-
+# parse reading books html
+reading_books = BeautifulSoup(reading_books_page.content, 'html.parser').find_all('tr', class_='book-preview')
 data = []
-for book in books:
+for book in reading_books:
     title_link = book.find('td', {'data-title': 'Title'}).find('a')
     title = title_link.text
     link = "https://bookwyrm.social" + title_link['href']
-    author_link = book.find('td', {'data-title': 'Author'}).find('a')
-    author = author_link.text
+    author = book.find('td', {'data-title': 'Author'}).find('a').text
+    date_started = book.find('td', {'data-title': 'Started'}).text.strip('\n ')
     img_url = book.find('img', class_='book-cover')['src']
-    data.append({'title': title, 'link': link, 'img_url': img_url, 'author': author})
+    data.append({
+      'title': title,
+      'link': link,
+      'img_url': img_url,
+      'author': author,
+      'date_started': date_started,
+    })
 
 with open('_data/reading.json', 'w') as f:
     json.dump(data, f)
